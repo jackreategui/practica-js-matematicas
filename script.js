9bd36f2c94ae9040d33398597ad6d7c2bf06d842
Jack Reategui Laptop <jackreategui2016@gmail.com>
2022-12-28 18:58:44 -0500

    añadi variables para sacar el area y el perimetro de un cuadrado, avance un poco del area del triangulo

---
+++ script.js
@@ -0,0 +1,17 @@
+const ladoCuadrado = 6;
+const perimetroCuadrado = ladoCuadrado * 4;
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+console.log({
+  ladoCuadrado,
+  perimetroCuadrado,
+  areaCuadrado,
+})
+
+const ladoTriangulo1 = 4;
+const ladoTriangulo2 = 4;
+const ladoTrianguloBase = 6;
+const alturaTriangulo = 7;
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase;
+const areaTriangulo = (ladoTrianguloBase * alturaTriangulo) / 2;
