ec22fd8b945b6458bd229f9a92d46a39ca9a3535
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-01-02 14:17:35 -0500

    termine la clase del triangulo y el cuadrado

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
+console.group("Cuadrado");
+
+// Cuadrado
+
 const ladoCuadrado = 6;
 const perimetroCuadrado = ladoCuadrado * 4;
 const areaCuadrado = ladoCuadrado * 2;
 
-console.log({
-  ladoCuadrado,
-  perimetroCuadrado,
-  areaCuadrado,
-})
+// console.log({
+//   ladoCuadrado,
+//   perimetroCuadrado,
+//   areaCuadrado,
+// })
+
+function calcularCuadrado(lado){
+  return{
+    perimetro: (lado * 4),
+    area: (lado * 2),
+  }
+}
+
+console.log(calcularCuadrado(3));
+
+console.groupEnd("Cuadrado");
+
+console.group("Triangulo");
+
+// Triangulo
 
 const ladoTriangulo1 = 4;
 const ladoTriangulo2 = 4;
@@ -15,3 +34,23 @@ const alturaTriangulo = 7;
 
 const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase;
 const areaTriangulo = (ladoTrianguloBase * alturaTriangulo) / 2;
+
+// console.log({
+//   ladoTriangulo1,
+//   ladoTriangulo2,
+//   ladoTrianguloBase,
+//   alturaTriangulo,
+//   perimetroTriangulo,
+//   areaTriangulo,
+// })
+
+function calcularTriangulo(lado1, lado2, base, altura){
+  return{
+    perimetro: (lado1 + lado2 + base),
+    area: (base * altura) / 2,
+  }
+}
+
+console.log(calcularTriangulo(5, 5, 6, 8));
+
+console.groupEnd("Triangulo");
