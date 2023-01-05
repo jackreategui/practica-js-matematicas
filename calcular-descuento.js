10a3184d80e0abb6c84008c87aaab3813f29d9e9
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-01-05 17:18:21 -0500

    termine la clase con el tema de encontrar el cupon mediante un array y objetos

--- calcular-descuento.js
+++ calcular-descuento.js
@@ -5,6 +5,28 @@ const pResult = document.querySelector('#result');
 
 button.addEventListener('click', calcularPrecioConDescuento);
 
+// Objeto para lograr el mismo resultado
+
+// const cuponObj = {
+//   'jack123': 30,
+//   'aji18': 12,
+//   '182is9': 40,
+// }
+
+const cuponesList = [];
+cuponesList.push({
+  name: 'jack123',
+  discount: 30,
+});
+cuponesList.push({
+  name: 'c32d23',
+  discount: 10,
+});
+cuponesList.push({
+  name: 'd3d25g5',
+  discount: 50,
+});
+
 function calcularPrecioConDescuento(){
   const price = Number(inputPrice.value);
   const cupon = inputCupon.value;
@@ -16,17 +38,37 @@ function calcularPrecioConDescuento(){
     return;
   }
 
-  switch (cupon) {
-    case ('J4ck'):
-      discount = 30;
-      break;
-    case ('J3#12'):
-      discount = 12;
-      break;
-    default:
-      pResult.innerHTML = 'No se encontraron cupones';
-      return;
+  function descuento(cuponElement){
+    return cuponElement.name == cupon;
+  };
+
+  const cuponArray = cuponesList.find(descuento);
+
+  if (cuponArray) {
+    discount = cuponArray.discount;
+  } else{
+    pResult.innerHTML = 'El cupon no es valido';
+    return;
   }
+
+  // if (cuponObj[cupon]) {
+  //   discount = cuponObj[cupon];
+  // } else{
+  //   pResult.innerHTML = 'El cupon no es valido';
+  //   return; 
+  // }
+
+  // switch (cupon) {
+  //   case ('J4ck'):
+  //     discount = 30;
+  //     break;
+  //   case ('J3#12'):
+  //     discount = 12;
+  //     break;
+  //   default:
+  //     pResult.innerHTML = 'No se encontraron cupones';
+  //     return;
+  // }
 
   // if (!cupon){
   //   pResult.innerHTML = 'No se encontraron cupones';
