c92c20362fccbc46c2b0f00012e31d094d110aa0
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-01-07 18:17:44 -0500

    termine clase de promedio usando .reduce y aprendiendo a usar funcione como variables

--- media_aritmetica.js
+++ media_aritmetica.js
@@ -1,15 +1,38 @@
-const salario = [212, 31121, 131, 311, 3532];
+//Metodo for
 
-function calcularSalarioPromedio(salarioLista){
+// const salario = [212, 31121, 131, 311, 3532];
 
-  let sumaSalario = 0;
+// function calcularSalarioPromedio(salarioLista){
+
+//   let sumaSalario = 0;
   
-  for (let i = 0; i < salarioLista.length; i++) {
-    sumaSalario = sumaSalario + salarioLista[i];
-  }
+//   for (let i = 0; i < salarioLista.length; i++) {
+//     sumaSalario = sumaSalario + salarioLista[i];
+//   }
   
-  const promedio = sumaSalario / salarioLista.length;
-  return promedio;
-};
+//   const promedio = sumaSalario / salarioLista.length;
+//   return promedio;
+// };
 
-console.log(calcularSalarioPromedio(salario));
+// console.log(calcularSalarioPromedio(salario));
+
+//Metodo .reduce
+
+// const salario = [2, 31, 11, 31, 32];
+
+// function calcularSalarioPromedio(lista){
+
+
+//   const sumaLista = lista.reduce((acumulado, elemento) => acumulado + elemento);
+
+//   const promedio = sumaLista / lista.length;
+//   return promedio;
+// }
+
+// console.log(calcularSalarioPromedio(salario));
+
+//Saber si un numreo es par
+
+function esPar(lista){
+
+}
