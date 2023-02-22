1b8219ddd1236d5dfb5320d0548f17c23b95a18a
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-02-22 17:32:02 -0500

    agregue nuevas funciones para ordenar las fechas de las empresas

--- analisis.js
+++ analisis.js
@@ -43,3 +43,23 @@ function aumentoSalarioPersona(nombrePersona) {
 
   return nuevoSalario;
 }
+
+// Analisis empresarial
+
+const empresas = {};
+
+for (persona of salarios) {
+  for (trabajo of persona.trabajos) {
+    if (!empresas[trabajo.empresa]) {
+      empresas[trabajo.empresa] = {};
+    }
+
+    if (!empresas[trabajo.empresa][trabajo.year]) {
+      empresas[trabajo.empresa][trabajo.year] = [];
+    }
+
+    empresas[trabajo.empresa][trabajo.year].push(trabajo.salario);
+  }
+}
+
+console.log({empresas});
