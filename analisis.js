1e0baea92c5054efa38f28daaddfc6dc6ca952e1
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-03-04 18:16:13 -0500

    termine la clase de mediana de empresas con salarios

--- analisis.js
+++ analisis.js
@@ -62,4 +62,14 @@ for (persona of salarios) {
   }
 }
 
-console.log({empresas});
+console.log({ empresas });
+
+function medianaEmpresaYear(nombre, year) {
+  if (!empresas[nombre]) {
+    console.warn('La empresa no existe');
+  } else if (!empresas[nombre][year]) {
+    console.warn('La empresa no dio salarios ese año');
+  } else {
+    return PlatziMath.calcularMediana(empresas[nombre][year]);
+  }
+}
